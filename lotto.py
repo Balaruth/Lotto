4ace7282b74196c0042286c9268af8cf019914ef
Balaruth <g.donoghue@outlook.com>
2018-07-17 21:01:33 +0200

    fixed error and included set to order numbers

--- lotto.py
+++ lotto.py
@@ -1,25 +1,30 @@
 import random
 
+numberlist = set()
+
 def number_input():
   print "Welcome to the lottery number generator."
   auth = True
   while auth == True:
     try:
       usernum = int(raw_input("Please enter how many numbers you would like to have: "))
-      lottery_gen(usernum)
+      lottery_gen(numberlist, usernum)
       auth = False
     except:
       print "Please enter a number."
 
-def lottery_gen(x):
-  numberlist = []
+
+def lottery_gen(x, y):
+
   counter = 0
-  while counter < x:
+  while counter < y:
     ball = random.randint(1, 45)
-    if ball not in numberlist:
-      numberlist.append(random.randint(1, 45))
+    if ball not in x:
+      x.add(ball)
       counter += 1
-  print "Here is your list of lottery numbers: " + str(numberlist)
+  lotto_ordered = list(x)
+  print "Here is your list of lottery numbers: " + str(lotto_ordered)
+
 
 if __name__ == '__main__':
   number_input()
